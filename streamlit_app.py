6d0d5e2ba865219e1268ce23f91199acbe2eedc8
Blacklotus88888 <132392928+Blacklotus88888@users.noreply.github.com>
2025-03-11 02:21:25 +0000

    init

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+main_page = st.Page("main_page.py", title="Main Page", icon="🎈")
+page_2 = st.Page("p2.py", title="Page 2", icon="❄️")
+page_3 = st.Page("p3.py", title="Page 3", icon="🎉")
+
+nav = st.navigation([main_page, page_2, page_3])
+
+st.sidebar.write("This is a sidebar")
+
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+
+nav.run()
+
